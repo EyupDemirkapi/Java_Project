824adb0c713edfcf6a305ccc3f206e2a3a41dd3d
EyupDemirkapi <eyupdemirkapi@marun.edu.tr>
2025-12-17 15:35:13 +0300

    BasicSınıfdeneme

---
+++ Java_Project/src/main/java/com/example/java_project/Öğrenci.java
@@ -0,0 +1,10 @@
+package com.example.java_project;
+
+public class Öğrenci extends Kullanıcı{
+    private String öğrenimYılı;
+    public Öğrenci(){}
+    public Öğrenci(String öğrenimYılı){
+        super();
+        this.öğrenimYılı = öğrenimYılı;
+    }
+}
---
+++ Java_Project/src/main/java/com/example/java_project/Öğretmen.java
@@ -0,0 +1,12 @@
+package com.example.java_project;
+
+public class Öğretmen extends Kullanıcı{
+    private String Uzmanlık;
+    public Öğretmen(){}
+    public Öğretmen(String uzmanlık){
+        super();
+        this.Uzmanlık = uzmanlık;
+    }
+
+
+}
