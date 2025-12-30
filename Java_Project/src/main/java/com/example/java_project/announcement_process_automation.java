26fd15bed6e22616350ae17c36b2952a5bf2e29f
EmreArslan18 <eibka8695@gmail.com>
2025-12-30 22:13:00 +0300

    sfsd

--- Java_Project/src/main/java/com/example/java_project/announcement_process_automation.java
+++ Java_Project/src/main/java/com/example/java_project/announcement_process_automation.java
@@ -9,13 +9,11 @@ import java.io.IOException;
 public class announcement_process_automation extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // 1. Burayı login-view.fxml olarak değiştiriyoruz
+
         FXMLLoader fxmlLoader = new FXMLLoader(announcement_process_automation.class.getResource("login-view.fxml"));
 
-        // 2. Sahne boyutunu giriş ekranına uygun yapalım (örneğin 400x300)
         Scene scene = new Scene(fxmlLoader.load(), 425, 600);
 
-        // 3. Başlığı giriş ekranına uygun şekilde güncelleyelim
         stage.setTitle("Duyuru Sistemi - Giriş");
         stage.setResizable(true);
         stage.setScene(scene);
